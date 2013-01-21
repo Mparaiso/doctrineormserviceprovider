33ec9ac9be30b0d4c1524db3b3730e025e0473d3
mparaiso <aikah@free.fr>
2013-01-21 19:05:48 +0100

    HelloBundle functional

--- src/Acme/HelloBundle/Controller/HiController.php
+++ src/Acme/HelloBundle/Controller/HiController.php
@@ -1,14 +1,24 @@
 <?php
+
 //@note @symfony créer un controller
 // src/Acme/HelloBundle/Controller/HiController.php
 
 namespace Acme\HelloBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class HiController{
-    function indexAction($name){
-        return new Response("<html><body>Hi '"
-            .$name."' !</body></html>");
+class HiController extends Controller {
+
+    function indexAction($name) {
+        // @note @symfony render a raw html response body
+        //return new Response("<html><body>Hi '"
+        //   .$name."' !</body></html>");
+        // @note @symfony render html with twig , class must extend Controller
+        return $this->render('AcmeHelloBundle:Hi:index.html.twig', array("name" => $name));
+        // @note @symfony rendre un template php 
+        // return $this->render('AcmeHelloBundle:Hi:index.html.php',array("name"=>$name));
+        // @note @symfony convention de nommage NomBundle:NomController:NomTemplate
     }
+
 }
---
+++ src/Acme/TestBundle/AcmeTestBundle.php
@@ -0,0 +1,11 @@
+<?php
+
+// src/Acme/TestBundle/AcmeTestBundle.php
+// @note @symfony créer un bundle manuellement
+namespace Acme\TestBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AcmeTestBundle extends Bundle{
+
+}
